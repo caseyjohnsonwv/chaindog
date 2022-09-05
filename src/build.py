8bcd3c6a4e783e4b08d60f031c49057f9930fcb7
Casey Johnson <caseyjohnsonwv@gmail.com>
2022-09-05 11:26:56 -0400

    build script improvements

--- src/build.py
+++ src/build.py
@@ -1,9 +1,17 @@
+import argparse
 import logging
 import os
 from pathlib import Path
 import shutil
 from subprocess import run
 from time import time
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--skip-dependencies", action=argparse.BooleanOptionalAction, help="Include this flag to skip building lambda layers.")
+parser.add_argument("-t", "--target", action="append", help="Target directory, can be passed more than once. If ommitted, all directories are built.")
+parser.set_defaults(target=[])
+ARGS = parser.parse_args()
 
 
 logging.basicConfig(
@@ -21,14 +29,16 @@ def main():
     os.mkdir(artifacts)
     c = 0
     for dir in Path(os.getcwd()).iterdir():
+        # skip artifcats directory
         if dir.is_dir() and dir.name != 'artifacts':
-            build_dir(dir)
-            c += 1
+            # enable target flag
+            if len(ARGS.target) == 0 or dir.name in ARGS.target:
+                c += build_dir(dir)
     tdelta = time() - start
     logger.info(f"Built {c} Lambdas in {tdelta:.1f} seconds")
 
 
-def build_dir(dir:Path):
+def build_dir(dir:Path) -> int:
     logger.info(f"Building {dir.name}")
     start = time()
     # create temp dir structure
@@ -39,6 +49,9 @@ def build_dir(dir:Path):
     req_file ='requirements-lambda.txt'
     reqs_target = "python/lib/python3.8/site-packages"
     if dir.joinpath(req_file).exists():
+        if ARGS.skip_dependencies:
+            logger.info(f"Skipping {dir.name} due to --skip-dependencies flag")
+            return 0
         # building lambda layer
         placeholder = temp_dir
         for part in reqs_target.split('/'):
@@ -47,17 +60,21 @@ def build_dir(dir:Path):
         shutil.copyfile(f"{dir.joinpath(req_file)}", f"{temp_dir.joinpath(req_file)}")
         # install linux requirements
         logger.debug(f"Using Docker to install requirements from {dir.name}/{req_file}")
-        command = f"docker run --name build -v {temp_dir.as_posix()}:/var/task public.ecr.aws/sam/build-python3.8 /bin/sh -c"
-        nosplit = [f"python -m pip install -r {req_file} --target {reqs_target}; exit"]
-        res = exec(command, nosplit)
-        command = f"docker container rm build"
-        exec(command)
-        # fail if build failed, but after deleting build container
-        res.check_returncode()
+        command = [
+            'docker', 'run', '--rm', '--name', 'build', '-v', f"{temp_dir.as_posix()}:/var/task", 'public.ecr.aws/sam/build-python3.8', '/bin/sh', '-c',
+            f"python -m pip install -r {req_file} --target {reqs_target}; exit"
+        ]
+        res = exec(command)
+        try:
+            res.check_returncode()
+        except:
+            logger.error(f"Failed to build {dir.name} - is the Docker daemon running?")
+            logger.error(res.stderr.decode('utf-8'))
+            exit(1)
     else:
         # packaging source code
         files = ' '.join([f"{dir.name}/{f.name}" for f in dir.iterdir()])
-        command = f"git check-ignore {files}"
+        command = ['git', 'check-ignore', files]
         res = exec(command)
         excludes = set(p.split('/')[-1] for p in res.stdout.decode('utf-8').split('\n') if len(p) > 0)
         logger.debug(f"Excluding {len(excludes)} .gitignore'd files")
@@ -73,12 +90,11 @@ def build_dir(dir:Path):
     shutil.rmtree(temp_dir)
     tdelta = time() - start
     logger.info(f"Built {dir.name} in {tdelta:.1f} seconds")
+    return 1
 
 
-def exec(cmd:str, nosplit:list=[]):
-    command = list(cmd.split(' '))
-    command.extend(nosplit)
-    return run(command, capture_output=True)
+def exec(cmd:list=[]):
+    return run(cmd, capture_output=True)
 
 
 if __name__ == '__main__':
