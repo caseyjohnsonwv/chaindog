fa9233f6768ccb6e4adc6f306073243f9ab515a3
Casey Johnson <caseyjohnsonwv@gmail.com>
2022-09-09 16:49:15 -0400

    set sqs option to null to fix terraform provider diff bug

--- infra/modules/data_ingestion/sqs.tf
+++ infra/modules/data_ingestion/sqs.tf
@@ -2,4 +2,5 @@ resource "aws_sqs_queue" "park_id_queue" {
   name                       = "park_id_queue_${var.env_name}"
   message_retention_seconds  = 180
   visibility_timeout_seconds = 180
+  sqs_managed_sse_enabled    = null
 }
